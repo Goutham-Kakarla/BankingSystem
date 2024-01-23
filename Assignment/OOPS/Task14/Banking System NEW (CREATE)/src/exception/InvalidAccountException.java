30cd55883128a5bfba0fc1acea2baad343a71a9f
Goutham Kakarla <gauthamkakarla346@gmail.com>
2024-01-23 11:12:12 +0530

    Delete Assignment/OOPS/Task14/Banking System NEW (CREATE) directory

--- Assignment/OOPS/Task14/Banking System NEW (CREATE)/src/exception/InvalidAccountException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InvalidAccountException extends RuntimeException {
-    public InvalidAccountException(String message) {
-        super(message);
-    }
-}
--- Assignment/OOPS/Task14/Banking System NEW (CREATE)/src/exception/OverDraftLimitExcededException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class OverDraftLimitExcededException extends RuntimeException {
-	public OverDraftLimitExcededException(String message)
-	{
-		super();
-	}
-}
